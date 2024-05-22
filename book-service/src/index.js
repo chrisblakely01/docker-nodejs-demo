70d5c9f6f104a3e2df16be68aefad8d91a8ac476
Chris Blakely <chrisblakely01@users.noreply.github.com>
2024-05-22 17:33:34 +0100

    adding steps

--- book-service/src/index.js
+++ book-service/src/index.js
@@ -1,25 +1,17 @@
 import express from "express";
+
+import Book from "./models/bookModel.js";
+import dbStart from "./db/db.js";
 import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
 dotenv.config();
 
-const books = [
-  { id: 1, title: "1984", author: "George Orwell" },
-  {
-    id: 2,
-    title: "Brave New World",
-    author: "Aldous Huxley",
-  },
-  {
-    id: 3,
-    title: "The Catcher in the Rye",
-    author: "J.D. Salinger",
-  },
-];
+dbStart();
 
 app.get("/books", async (req, res) => {
+  const books = await Book.find({});
   res.json(books);
 });
 
